ae5550126f3108cf4904720adcd3de3fa067b415
birdomi <ryk012@naver.com>
2024-09-04 09:18:58 +0900

    init

---
+++ train_linear.py
@@ -0,0 +1,103 @@
+import os
+import torch
+import torch.nn as nn
+
+import argparse
+import timm
+import numpy as np
+import utils
+
+import random
+import rein
+
+import dino_variant
+import disease_loader
+
+def train():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data', '-d', type=str)
+    parser.add_argument('--gpu', '-g', default = '0', type=str)
+    parser.add_argument('--netsize', default='s', type=str)
+    parser.add_argument('--save_path', '-s', type=str)
+    args = parser.parse_args()
+
+    device = 'cuda:'+args.gpu
+    save_path = os.path.join('checkpoints', args.save_path)
+    data_path = './data/'
+    batch_size = 32
+    max_epoch = 50
+    num_classes = 14
+
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
+
+    lr_decay = [int(0.5*max_epoch), int(0.75*max_epoch), int(0.9*max_epoch)]
+
+
+    train_loader, valid_loader = disease_loader.get_loader(data_path, batch_size = batch_size)
+        
+    if args.netsize == 's':
+        model_load = dino_variant._small_dino
+        variant = dino_variant._small_variant
+
+
+    model = torch.hub.load('facebookresearch/dinov2', model_load)
+    model.linear = nn.Linear(variant['embed_dim'], num_classes)
+    model.to(device)
+    
+    print(model)
+    criterion = torch.nn.CrossEntropyLoss()
+    model.eval()
+    
+    # optimizer = torch.optim.SGD(model.parameters(), lr = 0.01, momentum=0.9, weight_decay = 1e-05)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay = 1e-5)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, lr_decay)
+    saver = timm.utils.CheckpointSaver(model, optimizer, checkpoint_dir= save_path, max_history = 1) 
+    print(train_loader.dataset[0][0].shape)
+
+    # f = open(os.path.join(save_path, 'epoch_acc.txt'), 'w')
+    avg_accuracy = 0.0
+    for epoch in range(max_epoch):
+        ## training
+        model.train()
+        total_loss = 0
+        total = 0
+        correct = 0
+        for batch_idx, (inputs, targets) in enumerate(train_loader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            optimizer.zero_grad()
+            
+            with torch.no_grad():
+                outputs = model(inputs)
+            outputs = model.linear(outputs)
+            loss = criterion(outputs, targets)
+            loss.backward()            
+            optimizer.step()
+
+            total_loss += loss
+            total += targets.size(0)
+            _, predicted = outputs[:len(targets)].max(1)            
+            correct += predicted.eq(targets).sum().item()            
+            print('\r', batch_idx, len(train_loader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                        % (total_loss/(batch_idx+1), 100.*correct/total, correct, total), end = '')
+            train_accuracy = correct/total
+                  
+        train_avg_loss = total_loss/len(train_loader)
+        print()
+
+        ## validation
+        model.eval()
+        total_loss = 0
+        total = 0
+        correct = 0
+
+        valid_accuracy = utils.validation_accuracy(model, valid_loader, device, mode = 'linear')
+        scheduler.step()
+
+        saver.save_checkpoint(epoch, metric = valid_accuracy)
+        print('EPOCH {:4}, TRAIN [loss - {:.4f}, acc - {:.4f}], VALID [acc - {:.4f}]\n'.format(epoch, train_avg_loss, train_accuracy, valid_accuracy))
+        print(scheduler.get_last_lr())
+
+    
+if __name__ =='__main__':
+    train()
---
+++ utils/__init__.py
@@ -0,0 +1,2 @@
+from .metric import *
+from .dataset import *
